393ff0dff2e8d02c3f3c65521085926329f2e829
wky181 <1813721268@qq.com>
2019-07-26 17:44:18 +0800

    手动实现双链表已实现

--- src/数据结构/lanqiao/DoubleLinkList.java
+++ src/数据结构/lanqiao/DoubleLinkList.java
@@ -5,13 +5,15 @@ package 数据结构.lanqiao;
  * @date 2019/7/25 9:03
  * @Description:
  */
-public class DoubleLinkList implements  MyList{
+public class DoubleLinkList implements MyList {
     ListNode first;
     ListNode last;
     int size;
 
     public DoubleLinkList() {
+        //初始化前亚元
         first = new ListNode(null);
+        //初始化后亚元
         last = new ListNode(null);
         first.next = last;
         last.per = first;
@@ -21,7 +23,7 @@ public class DoubleLinkList implements  MyList{
     public String toString() {
         StringBuilder str = new StringBuilder("[");
         ListNode p = first.next;
-        while (p!=last){
+        while (p != last) {
             str.append(p.data);
             if (p != last.per) {
                 str.append(",");
@@ -31,10 +33,12 @@ public class DoubleLinkList implements  MyList{
         str.append(']');
         return str.toString();
     }
+
     @Override
     public void add(Object element) {
+        //在亚元之间插入一个元素  需要改变四个指针的指向
         ListNode newNode = new ListNode(element);
-        last.per.next= newNode;
+        last.per.next = newNode;
         newNode.per = last.per;
         newNode.next = last;
         last.per = newNode;
@@ -44,31 +48,99 @@ public class DoubleLinkList implements  MyList{
 
     @Override
     public void delete(Object element) {
+        ListNode p = first.next;
+        while (p != last) {
+            if (p.data.equals(element)) {
+                p.per.next = p.next;
+                p.next.per = p.per;
+                p.next = null;
+                p.per = null;
+                size--;
+                break;
+            }
+            p = p.next;
+        }
 
     }
 
     @Override
     public void delete(int index) {
-
+        if (index<0 || index>=size){
+            return;
+        }
+        ListNode p = first.next;
+        int i = 0;
+        while (p!=last){
+            if (i==index){
+                p.per.next = p.next;
+                p.next.per = p.per;
+                p.next = null;
+                p.per  = null;
+                size--;
+                break;
+            }
+            p = p.next;
+            i++;
+        }
     }
 
     @Override
     public void update(int index, Object newElement) {
-
+        if (index<0 || index>=size){
+            return ;
+        }
+        ListNode p = first.next;
+        int i = 0;
+        while (p != last) {
+            if (i == index) {
+                p.data = newElement;
+                break;
+            }
+            p = p.next;
+            i++;
+        }
     }
 
     @Override
     public boolean contains(Object target) {
+        ListNode p = first.next;
+        while (p != last) {
+            if (p.data.equals(target)){
+                return true;
+            }
+            p= p.next;
+        }
         return false;
     }
 
     @Override
     public int indexOf(Object str) {
-        return 0;
-    }
+        ListNode p = first.next;
+        int i = 0;
+        while (p != last) {
+            if (p.data.equals(str)){
+                return i;
+            }
+            p=p.next;
+            i++;
+        }
+        return -1;
+}
 
     @Override
     public Object at(int index) {
+        if (index<0 || index>=size){
+            return null;
+        }
+        ListNode p = first.next;
+        int i = 0;
+        while (p != last) {
+            if (i == index) {
+                return p.data;
+            }
+            p = p.next;
+            i++;
+        }
         return null;
     }
 }
--- src/数据结构/lanqiao/DoubleLinkListTest.java
+++ src/数据结构/lanqiao/DoubleLinkListTest.java
@@ -19,35 +19,38 @@ public class DoubleLinkListTest {
 
     @Test
     public void delete() {
-        SingleLinkedList singleLinkedList = new SingleLinkedList();
-        for (int i = 0; i < 10; i++) {
-            singleLinkedList.add("cxk" + i);
+        DoubleLinkList doubleLinkList  = new DoubleLinkList();
+        for (int i = 0; i < 10 ; i++) {
+            doubleLinkList.add("cxk"+i);
         }
-        singleLinkedList.delete(0);
-        singleLinkedList.delete(2);
-
-        System.out.println(singleLinkedList);
+        doubleLinkList.delete("cxk0");
+        System.out.println(doubleLinkList);
     }
 
 
     @Test
     public void update() {
-        SingleLinkedList singleLinkedList = new SingleLinkedList();
-        for (int i = 0; i < 10; i++) {
-            singleLinkedList.add("cxk" + i);
+        DoubleLinkList doubleLinkList  = new DoubleLinkList();
+        for (int i = 0; i < 10 ; i++) {
+            doubleLinkList.add("cxk"+i);
         }
-        singleLinkedList.update(8,"sadf");
-        singleLinkedList.update(6,"sadf");
-        System.out.println(singleLinkedList);
+        doubleLinkList.update(5,"wky");
+        System.out.println(doubleLinkList);
+
     }
 
     @Test
     public void contains() {
-        SingleLinkedList singleLinkedList = new SingleLinkedList();
-        for (int i = 0; i < 10; i++) {
-            singleLinkedList.add("cxk" + i);
+        DoubleLinkList doubleLinkList  = new DoubleLinkList();
+        for (int i = 0; i < 10 ; i++) {
+            doubleLinkList.add("cxk"+i);
         }
-        System.out.println(singleLinkedList.contains("cxk"));
+        doubleLinkList.delete(5);
+        doubleLinkList.update(5,"wky");
+        System.out.println(doubleLinkList.contains("wky"));
+        System.out.println(doubleLinkList.indexOf("wky"));
+        System.out.println(doubleLinkList.at(2));
+        System.out.println(doubleLinkList);
 
     }
 
